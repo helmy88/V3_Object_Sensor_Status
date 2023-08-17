7ee05e865cbbb83d5193c9365a0120f15d47e665
LOLZLINUX <69492477+helmy88@users.noreply.github.com>
2023-08-18 00:19:29 +0800

    Update Maker_Object_Sensor_status.ino

--- Maker_Object_Sensor_status.ino
+++ Maker_Object_Sensor_status.ino
@@ -1,30 +1,37 @@
-const int sensorPins[] = {2, 3, 4, 5, 6}; // Array of sensor pins
-const int numSensors = sizeof(sensorPins) / sizeof(sensorPins[0]); // Number of sensors
+int pin_FL = A2;
+int pin_FC = A4;
+int pin_FR = A0;
+int pin_L = A3;
+int pin_R = A5;
+
+int threshold = 500; // Adjust this threshold value based on your sensor characteristics
 
 void setup() {
   Serial.begin(9600); // Initialize Serial Monitor
-  
-  for (int i = 0; i < numSensors; i++) {
-    pinMode(sensorPins[i], INPUT); // Set sensor pins as input
-  }
 }
 
 void loop() {
-  for (int i = 0; i < numSensors; i++) {
-    checkSensor(i); // Call the function to check the sensor
-    delay(1000); // Delay before checking the next sensor
-  }
-}
-
-void checkSensor(int sensorIndex) {
-  int sensorState = digitalRead(sensorPins[sensorIndex]);
+  int reading_FL = analogRead(pin_FL);
+  int reading_FC = analogRead(pin_FC);
+  int reading_FR = analogRead(pin_FR);
+  int reading_L = analogRead(pin_L);
+  int reading_R = analogRead(pin_R);
   
-  // Display status based on sensor state
-  Serial.print("Sensor ");
-  Serial.print(sensorIndex + 1);
-  if (sensorState == HIGH) {
-    Serial.println(": Opponent Detected!");
-  } else {
-    Serial.println(": No Opponent Detected");
-  }
+  Serial.println("Sensor Statuses:");
+  Serial.print("Front Left: ");
+  Serial.println(reading_FL > threshold ? "Obstacle Detected!" : "No Obstacle Detected");
+  
+  Serial.print("Front Center: ");
+  Serial.println(reading_FC > threshold ? "Obstacle Detected!" : "No Obstacle Detected");
+  
+  Serial.print("Front Right: ");
+  Serial.println(reading_FR > threshold ? "Obstacle Detected!" : "No Obstacle Detected");
+  
+  Serial.print("Left: ");
+  Serial.println(reading_L > threshold ? "Obstacle Detected!" : "No Obstacle Detected");
+  
+  Serial.print("Right: ");
+  Serial.println(reading_R > threshold ? "Obstacle Detected!" : "No Obstacle Detected");
+  
+  delay(1000); // Delay to avoid rapid Serial Monitor output
 }
